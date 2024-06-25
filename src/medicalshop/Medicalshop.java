92b3b740008c8ac811e58c45af5f66fb974af05a
poojajungi <poojajungi@LAPTOP-UNQ8OCGQ>
2024-06-25 17:10:35 +0530

    23/06/2024 commit..

--- src/medicalshop/Medicalshop.java
+++ src/medicalshop/Medicalshop.java
@@ -14,9 +14,9 @@ public class Medicalshop extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
      
-        Parent root = FXMLLoader.load(getClass().getResource("imageForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("vieworder.fxml"));
         Scene scene = new Scene(root);
-   //    scene.getStylesheets().add(getClass().getResource("stock_design.css").toExternalForm());
+   //  scene.getStylesheets().add(getClass().getResource("register_design.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
     }
